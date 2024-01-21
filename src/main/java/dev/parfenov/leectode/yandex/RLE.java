18dfb0207421a1086b6e1e5a633ee7073dc19beb
kirillparfenov <toonart.ru@gmail.com>
2024-01-21 14:11:04 +0300

    yandex RLE

---
+++ src/main/java/dev/parfenov/leectode/yandex/RLE.java
@@ -0,0 +1,27 @@
+package dev.parfenov.leectode.yandex;
+
+// "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB"; // --> "A4B3C2XYZD4E3F3A6B28"
+public class RLE {
+    public static void main(String[] args) {
+        var s = "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB";
+        System.out.println(rle(s));
+    }
+
+    private static String rle(String s) {
+        var rle = new StringBuilder();
+        var counter = 0;
+        var previous = s.charAt(0);
+
+        for (var current : s.toCharArray()) {
+            if (current != previous) {
+                rle.append(previous).append(counter);
+                previous = current;
+                counter = 0;
+            }
+            counter++;
+        }
+
+        //последний подсчет тоже нужно указать, чтобы не потерялся
+        return rle.append(previous).append(counter).toString();
+    }
+}
