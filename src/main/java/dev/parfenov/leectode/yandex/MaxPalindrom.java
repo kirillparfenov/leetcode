a2e2125fe2c6d30e857fa07e35e2c407d79914d2
kirillparfenov <toonart.ru@gmail.com>
2024-01-21 12:29:05 +0300

    5. longest palindrom

--- src/main/java/dev/parfenov/leectode/yandex/MaxPalindrom.java
+++ src/main/java/dev/parfenov/leectode/yandex/MaxPalindrom.java
@@ -14,31 +14,40 @@ public class MaxPalindrom {
 //        var s = "aaacbabc";
 //        var s = "abb";
 //        var s = "a";
+//        var s = "aaaa";
+//        var s = "ccc";
         System.out.println(maxPalindrome(s));
     }
 
     private static String maxPalindrome(String s) {
-        var max = "";
+        var max = String.valueOf(s.charAt(0));
         int left, right;
         for (int i = 1; i < s.length(); i++) {
             if (i + 1 < s.length() && s.charAt(i - 1) == s.charAt(i + 1)) { //aba строка
                 left = i - 1;
                 right = i + 1;
-            } else if (s.charAt(i - 1) == s.charAt(i)) { //aa строка
-                left = i - 1;
-                right = i;
-            } else continue;
-
-            while (left - 1 >= 0 && right + 1 < s.length() && s.charAt(left - 1) == s.charAt(right + 1)) {
-                left--;
-                right++;
+                var current = findPalindrome(s, left, right);
+                if (current.length() > max.length())
+                    max = current;
             }
 
-            var current = s.substring(left, right + 1);
-            if (current.length() > max.length())
-                max = current;
+            if (s.charAt(i - 1) == s.charAt(i)) { //aa строка
+                left = i - 1;
+                right = i;
+                var current = findPalindrome(s, left, right);
+                if (current.length() > max.length())
+                    max = current;
+            }
         }
 
         return max;
+    }
+
+    private static String findPalindrome(String s, int left, int right) {
+        while (left - 1 >= 0 && right + 1 < s.length() && s.charAt(left - 1) == s.charAt(right + 1)) {
+            left--;
+            right++;
+        }
+        return s.substring(left, right + 1);
     }
 }
